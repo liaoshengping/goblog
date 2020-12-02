9c5e56ec72355049cab9b3219fdcc5e9fa6022a0
廖 <1194008361@qq.com>
2020-12-02 18:02:34 +0800

    3

--- main.go
+++ main.go
@@ -7,9 +7,15 @@ import (
 
 func main() {
 	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/2", handlerFunc2)
 	http.ListenAndServe(":3000", nil)
 }
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello,world</h1>")
+func handlerFunc(W http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(W, "<h1>Hello,world</h1>")
+	r.Header.Get("User-Agent")
+}
+
+func handlerFunc2(W http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(W, "<h1>Hello,world2</h1>")
 }
